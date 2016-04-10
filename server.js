a893dd3f6859c6058746b44623feaf1734dfe022
Gabriel Rodrigues <Gabriel Rodrigues>
2016-04-10 15:26:30 -0300

    Adicionado o método PUT para atualização de registro utilizando o id como referência

--- server.js
+++ server.js
@@ -32,55 +32,56 @@ router.get("/", function (req, res) {
 // POST, etc. use o route() para remover código redundante e enxutar o script.
 
 router.route("/users").get(function (req, res) {
-  console.log("Requisitando: GET /users");
-  var response = {};
-  mongoOp.find({}, function (err, data) {
-    if (err) {
-      console.log("Erro ao tentar requisitar dados do MongoDB\nErro:", err);
-      response = {
-        "error": true,
-        "message": "Error fetching the data"
-      };
-    } else {
-      console.log("Respondendo requisição com seguinte dados:", data);
-      response = {
-        "error": false,
-        "message": data
-      };
-    }
-    res.json(response);
-  });
-}).post(function (req, res) {
-  console.log("Requisitando: POST /users\nDados da requisição:", req);
-  var db = new mongoOp();
-  var response = {};
-  //fetch email & password from REST request.
-  // Add Strict validation when you use this in Production.
-  db.userEmail = req.body.email;
-  // Hash the password using SHA1 algorithm.
-  db.userPassword = require('crypto')
-    .createHash('sha1')
-    .update(req.body.password)
-    .digest('base64');
-  db.save(function (err) {
-    // save() will run the MongoDB insert() command.
-    // and will add new data in conllection.
-    if (err) {
-      console.log("Erro enquanto tentava salvar dados\nErro:", err);
-      response = {
-        "error": true,
-        "message": "Error adding data"
-      };
-    } else {
-      console.log("Dados salvos, respondendo ao cliente.\n");
-      response = {
-        "error": false,
-        "message": "Data added"
-      };
-    }
-    res.json(response);
-  });
-}); // route /users, GET e POST
+    console.log("Requisitando: GET /users");
+    var response = {};
+    mongoOp.find({}, function (err, data) {
+      if (err) {
+        console.log("Erro ao tentar requisitar dados do MongoDB\nErro:", err);
+        response = {
+          "error": true,
+          "message": "Error fetching the data"
+        };
+      } else {
+        console.log("Respondendo requisição com seguinte dados:", data);
+        response = {
+          "error": false,
+          "message": data
+        };
+      }
+      res.json(response);
+    });
+  })
+  .post(function (req, res) {
+    console.log("Requisitando: POST /users\nDados da requisição:", req);
+    var db = new mongoOp();
+    var response = {};
+    //fetch email & password from REST request.
+    // Add Strict validation when you use this in Production.
+    db.userEmail = req.body.email;
+    // Hash the password using SHA1 algorithm.
+    db.userPassword = require('crypto')
+      .createHash('sha1')
+      .update(req.body.password)
+      .digest('base64');
+    db.save(function (err) {
+      // save() will run the MongoDB insert() command.
+      // and will add new data in conllection.
+      if (err) {
+        console.log("Erro enquanto tentava salvar dados\nErro:", err);
+        response = {
+          "error": true,
+          "message": "Error adding data"
+        };
+      } else {
+        console.log("Dados salvos, respondendo ao cliente.\n");
+        response = {
+          "error": false,
+          "message": "Data added"
+        };
+      }
+      res.json(response);
+    });
+  }); // route /users, GET e POST
 
 router.route("/users/:id")
   .get(function (req, res) {
@@ -103,6 +104,51 @@ router.route("/users/:id")
       }
       res.json(response);
     })
+  })
+  .put(function (req, res) {
+    console.log("Requisição: PUT /users/" + req.params.id);
+    var response = {};
+    // first find out record exists or not
+    // if it does then update the record
+    mongoOp.findById(req.params.id, function (err, data) {
+      if (err) {
+        console.log("Erro ao tentar requisitar dados do MongoDB\nErro:", err);
+        response = {
+          "error": true,
+          "message": "Error fetching data"
+        };
+      } else {
+        console.log("Registro " + req.params.id + "encontrado para ser atualizado");
+        // we've got data from Mongo.
+        // change it accordingly
+        if (req.body.userEmail !== undefined) {
+          console.log("Atualizando email");
+          // case where email needs to be updated
+          data.userEmail = req.body.userEmail;
+        }
+        if (req.body.userPassword !== undefined) {
+          console.log("Atualizando senha");
+          // case where password need to be updated
+          data.userPassword = req.body.userPassword;
+        }
+        data.save(function (err) {
+          if (err) {
+            console.log("Erro ao tentar atualizar os dados do registro " + req.params.id + " de usuário");
+            response = {
+              "error": true,
+              "message": "Error updating data"
+            };
+          } else {
+            console.log("Dados do registro " + req.params.id + "atualizados.");
+            response = {
+              "error": true,
+              "message": "Data updated for " + req.params.id
+            };
+          }
+          res.json(response);
+        });
+      }
+    });
   }); // route GET /users/:id
 
 app.use('/', router);
