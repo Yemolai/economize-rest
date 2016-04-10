cf6fb2f09a80bb75abb18a4d93101225ae8b033c
Gabriel Rodrigues <Gabriel Rodrigues>
2016-04-10 00:38:56 -0300

    Adicionando requisição GET de usuário específico e verbose debug

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var bodyParser = require("body-parser");
 var router = express.Router();
 // Objeto do mongoose para manipulação do MongoDB
 var mongoOp = require("./models/mongo");
-
+console.log("Inicializando\n\n...\n");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    "extended": false
+  "extended": false
 }));
 
 /*
@@ -19,63 +19,93 @@ app.use(bodyParser.urlencoded({
     DELETE /users/:id Delete particular user
 */
 
-router.get("/", function(req, res) {
-    res.json({
-        "error": false,
-        "message": "No methods working right now"
-    });
+router.get("/", function (req, res) {
+  console.log("Requisitando: GET / \nDados da requisição:", req);
+  res.json({ // respondendo com mensagem simpática
+    "error": false,
+    "message": "Hi, how are you?"
+  });
 });
 
 // route() vai permitir que você use o mesmo caminho para diferentes operações
 // HTTP. Então se você tem a mesma URL com diferentes operações HTTP como GET,
 // POST, etc. use o route() para remover código redundante e enxutar o script.
 
-router.route("/users").get(function(req, res) {
-    var response = {};
-    mongoOp.find({}, function(err, data) {
-        if (err)
-            response = {
-                "error": true,
-                "message": "Error fetching the data"
-            };
-        else
-            response = {
-                "error": false,
-                "message": data
-            };
+router.route("/users").get(function (req, res) {
+  console.log("Requisitando: GET /users \nDados da requisição:", req);
+  var response = {};
+  mongoOp.find({}, function (err, data) {
+    if (err) {
+      console.log("Erro ao tentar requisitar dados do MongoDB\nErro:", err);
+      response = {
+        "error": true,
+        "message": "Error fetching the data"
+      };
+    } else {
+      console.log("Respondendo requisição com seguinte dados:", data);
+      response = {
+        "error": false,
+        "message": data
+      };
+    }
+    res.json(response);
+  });
+}).post(function (req, res) {
+  console.log("Requisitando: POST /users\nDados da requisição:", req);
+  var db = new mongoOp();
+  var response = {};
+  //fetch email & password from REST request.
+  // Add Strict validation when you use this in Production.
+  db.userEmail = req.body.email;
+  // Hash the password using SHA1 algorithm.
+  db.userPassword = require('crypto')
+    .createHash('sha1')
+    .update(req.body.password)
+    .digest('base64');
+  db.save(function (err) {
+    // save() will run the MongoDB insert() command.
+    // and will add new data in conllection.
+    if (err) {
+      console.log("Erro enquanto tentava salvar dados\nErro:", err);
+      response = {
+        "error": true,
+        "message": "Error adding data"
+      };
+    } else {
+      console.log("Dados salvos, respondendo ao cliente.\n");
+      response = {
+        "error": false,
+        "message": "Data added"
+      };
+    }
+    res.json(response);
+  });
+}); // route /users, GET e POST
 
-        res.json(response);
-    });
-}).post(function(req, res) {
-    var db = new mongoOp();
+router.route("/users/:id")
+  .get(function (req, res) {
+    console.log("Requisição: GET /users/:id \nDados da requisição:", req);
     var response = {};
-    //fetch email & password from REST request.
-    // Add Strict validation when you use this in Production.
-    db.userEmail = req.body.email;
-    // Hash the password using SHA1 algorithm.
-    db.userPassword = require('crypto')
-        .createHash('sha1')
-        .update(req.body.password)
-        .digest('base64');
-    db.save(function(err) {
-        // save() will run the MongoDB insert() command.
-        // and will add new data in conllection.
-        if (err)
-            response = {
-                "error": true,
-                "message": "Error adding data"
-            };
-        else
-            response = {
-                "error": false,
-                "message": "Data added"
-            };
-        res.json(response);
-    });
-});
-
+    mongoOp.findById(req.parameters.id, function (err, data) {
+      // This will run Mongo Query to fetch data based on ID
+      if (err) {
+        console.log("Erro ao tentar requisitar dados do MongoDB\nErro:", err);
+        response = {
+          "error": true,
+          "message": "Error fetching the database"
+        };
+      } else {
+        console.log("Respondendo requisição com seguinte dados:", data);
+        response = {
+          "error": false,
+          "message": data
+        };
+      }
+      res.json(response);
+    })
+  }); // route GET /users/:id
 
 app.use('/', router);
 
 app.listen(3000);
-console.log("Listening to PORT 3000");
+console.log("Servidor ativo na porta 3000");
